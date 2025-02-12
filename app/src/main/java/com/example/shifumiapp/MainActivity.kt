1a5a55194e883b90f980c07d062ad997559c2122
Atronche-riviere <axel.troncheriviere@etu.unilim.fr>
2025-02-12 16:50:27 +0100

    Implemented navigation between home and play

--- app/src/main/java/com/example/shifumiapp/MainActivity.kt
+++ app/src/main/java/com/example/shifumiapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.shifumiapp.ui.theme.ShiFuMiAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +28,65 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShiFuMiAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShiFuMiAppTheme {
-        Greeting("Android")
+fun AppNavigation(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+        modifier = modifier
+    ) {
+        composable("home") { HomeScreen(navController) }
+        composable("play") { PlayScreen(navController) }
     }
 }
+
+@Composable
+fun PlayScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Retour",
+            modifier = Modifier
+                .clickable { navController.navigate("home") }
+                .padding(8.dp)
+        )
+
+    }
+}
+
+
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Bienvenue",
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Text(
+            text = "Jouer",
+            modifier = Modifier
+                .clickable{navController.navigate("play")}
+        )
+    }
+}
+
+
